08bdabd4a37d3de2f4180624bc4e581578ecd290
mannguyenngocprovip <151109204+mannguyenngocprovip@users.noreply.github.com>
2023-12-04 11:22:09 +0700

    [DichVu] - Thêm dịch vụ

--- QLKS/Program.cs
+++ QLKS/Program.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace QLKS
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -16,7 +16,7 @@ namespace QLKS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmTranglogin());
+            Application.Run(new Form1());
         }
     }
 }
